a2e86cdca23a75fea0bf0a1cbfb006c24b221258
manwaring <phillip.manwaring@gmail.com>
2020-05-06 22:08:01 -0400

    update invalidations code

--- src/invalidate-cache.ts
+++ src/invalidate-cache.ts
@@ -1,29 +1,27 @@
-import "source-map-support/register";
-import { CloudFront } from "aws-sdk";
-import { sns, SnsSignature } from "@manwaring/lambda-wrapper";
+import 'source-map-support/register';
+import { CloudFront } from 'aws-sdk';
+import { sns, SnsSignature } from '@manwaring/lambda-wrapper';
 
-const cloudFront = new CloudFront({ apiVersion: "2016-11-25" });
+const cloudFront = new CloudFront({ apiVersion: '2019-03-26' });
 
-export const handler = sns(
-  async ({ message, success, error }: SnsSignature) => {
-    try {
-      const { keys } = message;
-      await invalidateCDN(keys);
-      return success("Successfully invalidated CDN");
-    } catch (err) {
-      return error(err);
-    }
+export const handler = sns(async ({ message, success, error }: SnsSignature) => {
+  try {
+    const { keys } = message;
+    await invalidateCDN(keys);
+    return success('Successfully invalidated CDN');
+  } catch (err) {
+    return error(err);
   }
-);
+});
 
 function invalidateCDN(Items: string[]): Promise<any> {
   const params = {
     DistributionId: process.env.CDN,
     InvalidationBatch: {
       CallerReference: new Date().getTime().toString(),
-      Paths: { Quantity: 1, Items },
+      Paths: { Quantity: Items.length, Items },
     },
   };
-  console.info("Invalidating CDN with params", params);
+  console.info('Invalidating CDN with params', JSON.stringify(params));
   return cloudFront.createInvalidation(params).promise();
 }
