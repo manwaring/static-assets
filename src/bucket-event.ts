1f39e932b8dae29ef7829d5f49f093390b819a80
manwaring <phillip.manwaring@gmail.com>
2020-05-06 21:43:26 -0400

    organize repo assets

--- src/bucket-event.ts
+++ src/bucket-event.ts
@@ -1,23 +1,29 @@
 import "source-map-support/register";
 import { SNS } from "aws-sdk";
+import { S3Event } from "aws-lambda";
 import { wrapper, WrapperSignature } from "@manwaring/lambda-wrapper";
 
 const sns = new SNS({ apiVersion: "2010-03-31" });
 
-export const handler = wrapper(async ({ success, error }: WrapperSignature) => {
-  try {
-    await publishSiteUpdatedEvent();
-    return success();
-  } catch (err) {
-    return error(err);
+export const handler = wrapper(
+  async ({ event, success, error }: WrapperSignature) => {
+    try {
+      const keys = (event as S3Event).Records.map(
+        (event) => event.s3.object.key
+      );
+      await publishAssetUpdatedEvent(keys);
+      return success();
+    } catch (err) {
+      return error(err);
+    }
   }
-});
+);
 
-function publishSiteUpdatedEvent(): Promise<any> {
+function publishAssetUpdatedEvent(keys: string[]): Promise<any> {
   const params = {
     TopicArn: process.env.SITE_UPDATED_TOPIC,
-    Message: JSON.stringify("Updated"),
+    Message: JSON.stringify({ keys }),
   };
-  console.log("Publishing site updated event with params", params);
+  console.log("Publishing asset updated event with params", params);
   return sns.publish(params).promise();
 }
--- src/invalidate-cache.ts
+++ src/invalidate-cache.ts
@@ -4,21 +4,24 @@ import { sns, SnsSignature } from "@manwaring/lambda-wrapper";
 
 const cloudFront = new CloudFront({ apiVersion: "2016-11-25" });
 
-export const handler = sns(async ({ success, error }: SnsSignature) => {
-  try {
-    await invalidateCDN();
-    return success("Successfully invalidated CDN");
-  } catch (err) {
-    return error(err);
+export const handler = sns(
+  async ({ message, success, error }: SnsSignature) => {
+    try {
+      const { keys } = message;
+      await invalidateCDN(keys);
+      return success("Successfully invalidated CDN");
+    } catch (err) {
+      return error(err);
+    }
   }
-});
+);
 
-function invalidateCDN(): Promise<any> {
+function invalidateCDN(Items: string[]): Promise<any> {
   const params = {
     DistributionId: process.env.CDN,
     InvalidationBatch: {
       CallerReference: new Date().getTime().toString(),
-      Paths: { Quantity: 1, Items: ["*"] },
+      Paths: { Quantity: 1, Items },
     },
   };
   console.info("Invalidating CDN with params", params);
